a86ac1283670e148c44af21abd5b18e5e34d62c0
Mathias Bellerby <Mathis2501@gmail.com>
2016-09-09 12:52:06 +0200

    6 Pass 9 Don't

---
+++ 03_Simon_Says/Simon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Simon_Says
+{
+    class Simon
+    {
+        public static string Echo(string EchoThis)
+        {
+            return EchoThis.ToLower();
+        }
+
+        public static string Shout(string EchoThisUpper)
+        {
+            return EchoThisUpper.ToUpper();
+        }
+
+        public static string Repeat(string Repeat)
+        {
+            return Repeat + " " + Repeat;
+        }
+        public static string Repeat(string RetString, int RetInt)
+        {
+            string OrigString = RetString;
+            for (int i = 1; i < RetInt; i++)
+            {
+                RetString = RetString + " " + OrigString;
+            }
+            return RetString;
+        }
+
+        public static string StartOfWord(string RetString, int RetInt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string FirstWord(string OrigString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Titleize(string OrigString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- 03_Simon_Says/SimonSaysTest.cs
+++ 03_Simon_Says/SimonSaysTest.cs
@@ -16,31 +16,31 @@ namespace _03_Simon_Says
         public void ShouldEchoHello()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("hello", simon.Echo("hello"));
+            Assert.AreEqual("hello", Simon.Echo("hello"));
         }
         [TestMethod]
         public void ShouldEchoBye()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("bye", simon.Echo("Bye"));
+            Assert.AreEqual("bye", Simon.Echo("Bye"));
         }
         [TestMethod]
         public void ShoutHello()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("HELLO", simon.Shout("hello"));
+            Assert.AreEqual("HELLO", Simon.Shout("hello"));
         }
         [TestMethod]
         public void ShoutMultipleWords()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("HELLO WORLD", simon.Shout("hello world"));
+            Assert.AreEqual("HELLO WORLD", Simon.Shout("hello world"));
         }
         [TestMethod]
         public void ShouldRepeat()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("hello hello", simon.Repeat("hello"));
+            Assert.AreEqual("hello hello", Simon.Repeat("hello"));
         }
 
 
@@ -53,71 +53,65 @@ namespace _03_Simon_Says
         public void ShouldRepeatANumberOfTimes()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("hello hello hello", simon.Repeat("hello", 3));
+            Assert.AreEqual("hello hello hello", Simon.Repeat("hello", 3));
         }
 
-        [TestMethod]
-        public void ShouldRepeatANumberOfTimes()
-        {
-            Simon simon = new Simon();
-            Assert.AreEqual("hello hello hello", simon.Repeat("hello", 3));
-        }
         [TestMethod]
         public void ReturnsFirstLetterOfWord()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("h", simon.StartOfWord("hello", 1));
+            Assert.AreEqual("h", Simon.StartOfWord("hello", 1));
         }
         [TestMethod]
         public void ReturnsFirstTwoLettersOfWord()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("Bo", simon.StartOfWord("Bob", 2));
+            Assert.AreEqual("Bo", Simon.StartOfWord("Bob", 2));
         }
         [TestMethod]
         public void ReturnsFirstSeveralLettersOfWord()
         {
             Simon simon = new Simon();
             string s = "abcdefg";
-            Assert.AreEqual("a", simon.StartOfWord(s, 1));
-            Assert.AreEqual("ab", simon.StartOfWord(s, 2));
-            Assert.AreEqual("abc", simon.StartOfWord(s, 3));
+            Assert.AreEqual("a", Simon.StartOfWord(s, 1));
+            Assert.AreEqual("ab", Simon.StartOfWord(s, 2));
+            Assert.AreEqual("abc", Simon.StartOfWord(s, 3));
         }
         [TestMethod]
         public void ReturnsFirstWordOfHelloWorld()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("Hello", simon.FirstWord("Hello World"));
+            Assert.AreEqual("Hello", Simon.FirstWord("Hello World"));
         }
         [TestMethod]
         public void ReturnsFirstWordOfOhDear()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("Oh", simon.FirstWord("Oh Dear"));
+            Assert.AreEqual("Oh", Simon.FirstWord("Oh Dear"));
         }
         [TestMethod]
         public void CapitilizeAWord()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("Jaws", simon.Titleize("jaws"));
+            Assert.AreEqual("Jaws", Simon.Titleize("jaws"));
         }
         [TestMethod]
         public void CapitilizeEveryWord()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("David Copperfield", simon.Titleize("david copperfield"));
+            Assert.AreEqual("David Copperfield", Simon.Titleize("david copperfield"));
         }
         [TestMethod]
         public void DontCapitilizeLittleWords()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("War and Peace", simon.Titleize("war and peace"));
+            Assert.AreEqual("War and Peace", Simon.Titleize("war and peace"));
         }
         [TestMethod]
         public void CapitilizeLittleWordsInBeginningOfSentence()
         {
             Simon simon = new Simon();
-            Assert.AreEqual("The Bridge over the River Kwai", simon.Titleize("the bridge over the river kwai"));
+            Assert.AreEqual("The Bridge over the River Kwai", Simon.Titleize("the bridge over the river kwai"));
         }
     }
 }
